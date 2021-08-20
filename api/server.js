90a2798530c4ce54615066e550392ee397950e68
matt22881 <matt22881@gmail.com>
2021-08-20 16:52:32 -0400

    edit for postgres

--- api/server.js
+++ api/server.js
@@ -7,6 +7,8 @@ const authRouter = require('./auth/auth-router')
 const plantsRouter = require('./plants/plants-router')
 const usersRouter = require('./users/users-router')
 
+const dbEnv = process.env.NODE_ENV
+
 const server = express()
 
 server.use(cors())
@@ -19,10 +21,16 @@ server.use('/api/plants', plantsRouter)
 server.use('/api/users', usersRouter)
 
 server.use((err, req, res, next) => { // eslint-disable-line
-    res.status(err.status || 500).json({
-        message: err.message,
-        stack: err.stack,
-    });
-});
 
+    if (dbEnv === "production") {
+        res.status(err.status || 500).json({
+            message: err.message
+        })
+    } else {
+        res.status(err.status || 500).json({
+            message: err.message,
+            stack: err.stack,
+        });
+    };
+})
 module.exports = server
