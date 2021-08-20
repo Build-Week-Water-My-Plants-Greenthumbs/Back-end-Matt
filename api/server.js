35eccb739e2e17aa82089bf266183bb36932f502
matt22881 <matt22881@gmail.com>
2021-08-20 16:27:09 -0400

    adds error handling in server.js

--- api/server.js
+++ api/server.js
@@ -18,4 +18,11 @@ server.use('/api/auth', authRouter)
 server.use('/api/plants', plantsRouter)
 server.use('/api/users', usersRouter)
 
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack,
+    });
+});
+
 module.exports = server
